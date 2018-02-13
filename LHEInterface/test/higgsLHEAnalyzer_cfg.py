f9be07ddd5c91a8790b100663b8914887d9fed77
SangEun Lee <d4space@gmail.com>
2018-02-13 21:55:02 +0900

    ahaha

---
+++ LHEInterface/test/higgsLHEAnalyzer_cfg.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("higgsLHE")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring('file:MCDBtoEDM_NONE.root')
+    #fileNames = cms.untracked.vstring(
+    #    'file:myfile.root'
+    #)
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("higgsLHEAnal.root"),
+    #closeFileFast = cms.untracked.bool(True)
+    )
+
+process.hAna = cms.EDAnalyzer('higgsLHEAnalyzer',
+    src = cms.InputTag("source")
+)
+
+
+process.p = cms.Path(process.hAna)
