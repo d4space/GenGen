89f5a226af157cfffdc6747db4acffdc626e69dc
SangEun Lee <d4space@gmail.com>
2018-05-01 14:49:31 +0200

    hahaha

--- GridPacks/getTestRunAndSub.sh
+++ GridPacks/getTestRunAndSub.sh
@@ -1,4 +1,7 @@
 #wget https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIIFall17wmLHEGS-01668
 #chmod u+x HIG-RunIIFall17wmLHEGS-01668
+##################################################################################
+# If you  want to run this interactivley, you should run this outside of CMSSW
+##################################################################################
 bsub -q 8nh HIG-RunIIFall17wmLHEGS-01668
 
